4b1b61f4acf38784f9cff2d8916c5312b678100f
Masad Moral <96681609+MasadMoral@users.noreply.github.com>
2024-10-07 18:16:53 +0600

    Create answer.py

---
+++ answer.py
@@ -0,0 +1,16 @@
+{
+  "youtube": {
+    "Watch YouTube Video #1": "070624",
+    "Major Games #1": "070624",
+    "Watch YouTube Shorts #3": "070624",
+    "Watch YouTube Shorts #4": "159390",
+    "Watch YouTube Shorts #6": "241086"
+  },
+  "answer": {
+    "choice_1": 12,
+    "choice_2": 3,
+    "choice_3": 14,
+    "choice_4": 5
+  },
+  "expires": 1728320776
+}
